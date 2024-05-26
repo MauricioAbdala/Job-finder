f245cbb9f4c2eb5550d5f6518d15a24b141a65d0
Mauricio Abdala <mauricio.abdala@outlook.com>
2024-05-25 21:19:28 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const app = express();
+const path = require('path')
+const db = require('./db/connection')
+const bodyParser = require('body-parser');
+const Job = require('./models/Job');
+const Sequelize = require('sequelize');
+const OP = Sequelize.Op;
+
+const PORT = 3000;
+
+
+
+app.listen(PORT, function () {
+    console.log(`O Express está rodando na porta ${PORT}`)
+});
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// handle bars
+app.engine('hbs', engine({ extname: '.hbs', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+// static folders
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("Conectou ao banco com sucesso");
+    })
+    .catch(err => {
+        console.log("Ocorreu um erro ao conectar", err)
+    });
+
+//routes
+app.get('/', (req, res) => {
+
+    let search = req.query.job;
+    let query = '%' +search+ '%'; //PH -> PHP, Word -> Wordpress, press -> Wordpress
+
+    if (!search) {
+        Job.findAll({
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        })
+            .then(jobs => {
+                console.log(jobs); // Verificar estrutura dos dados
+                res.render('index', {
+                    jobs
+                });
+            })
+            .catch(err => console.log(err));
+    } else {
+        Job.findAll({
+            where: {title: {[OP.like]: query}},
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        })
+            .then(jobs => {
+                console.log(jobs); // Verificar estrutura dos dados
+                res.render('index', {
+                    jobs, search
+                });
+            })
+            .catch(err => console.log(err));
+    }
+});
+
+
+
+  
+    // let query = '%' +search+ '%'; //PH -> PHP, Word -> Wordpress, press -> Wordpress
+
+    
+    app.get('/jobs', (req, res) => {
+        Job.findAll({
+            order: [['createdAt', 'DESC']]
+        })
+        .then(jobs => {
+            console.log(jobs);
+            res.render('jobs', { jobs });
+        })
+        .catch(err => console.log(err));
+    });
+       
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
+
+
+
